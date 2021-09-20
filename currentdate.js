12d3520cab9d527655418e5a5509ce857d6392f1
EQA2018 <smi17051@byui.edu>
2021-09-20 17:38:25 -0600

    Fixed links for js with tutor

--- currentdate.js
+++ currentdate.js
@@ -26,8 +26,10 @@ const dayName = daynames[d.getDay()];
 const monthName = months[d.getMonth()];
 const year = d.getFullYear();
 //const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+const fulldate = `${dayName}, ${d.getDate()}, ${monthName} ${year}`;
 document.getElementById("currentdate").textContent = fulldate;
 const fullyear = `${year}`;
 document.getElementById("copyrightyear").textContent = fullyear;
+
+//const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
 // ****************************
